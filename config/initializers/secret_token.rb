f5e5a6f110519c90ed458a9c82ef6b3230396649
Jun Lim <junyuen@gmail.com>
2014-08-01 10:24:56 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mosseryco::Application.config.secret_key_base = '90c9af9b5a65a3ff1525051aef28f9340f4cef4dcaf61f32783995c51a348ee645411e7020b3f19c5ac03cc67a1890c051c92ed42945b0d63b13578f426f5955'
